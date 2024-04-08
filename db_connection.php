79cbffcc629f9e1d13b2a5bf3b3d8e9a94f7871e
LokeshKumarKorampalli <korampallilokesh@gmail.com>
2024-04-08 09:29:19 +0530

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,15 +1,29 @@
 <?php
-// Database connection details
-$servername = "localhost";
-$username = "root";
-$password = "12344321";
-$dbname = "webtea";
+// Load .env file
+$env_file = __DIR__ . '/.env';
+if (file_exists($env_file)) {
+    $lines = file($env_file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (strpos(trim($line), '#') !== 0) {
+            list($key, $value) = explode('=', $line, 2);
+            $_ENV[trim($key)] = trim($value);
+        }
+    }
+}
+
+
+
+$db_host = $_ENV['DB_HOST'];
+$db_user = $_ENV['DB_USER'];
+$db_pass = $_ENV['DB_PASS'];
+$db_name = $_ENV['DB_NAME'];
+
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($db_host ,$db_user, $db_pass, $db_name);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-?>
+
